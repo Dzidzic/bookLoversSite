4e3d406fe22de08bdc1dad6c22ffd1949a80c627
Dzidzic <mdzidzic07@gmail.com>
2024-08-03 23:19:05 +0200

    Created Simple Navbar

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,70 @@
+<script>
+	import { page } from "$app/stores";
+
+    let isLoggedIn = false;
+    let openNavbar = false;
+    $: currentPage = $page.url.pathname;
+
+    function changeNavbar(){
+        openNavbar = !openNavbar;
+    }
+</script>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="/">Book Lover</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarNav"
+			aria-controls="navbarNav"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+            on:click={changeNavbar}
+		>
+			<span class="navbar-toggler-icon" />
+		</button>
+		<div class:show = {openNavbar} class="collapse navbar-collapse" id="navbarNav">             
+            <ul class="navbar-nav">
+                {#if isLoggedIn}
+    				<li class="nav-item">
+    					<a class:active = {currentPage == '/'} class="nav-link" aria-current="page" href="/">Home</a>
+				    </li>
+				    <li class="nav-item">
+    					<a class:active = {currentPage == '/add'} class="nav-link" href="/add">Add Book</a>
+				    </li>
+				    <li class="nav-item">
+					    <a class:active = {currentPage == 'profile'} class="nav-link" href="/profile">Profile</a>
+				    </li>
+				    <li class="nav-item">
+    					<a class:active = {currentPage == '/about'} class="nav-link" href="/about">About</a>
+	    			</li>
+		    		<li class="nav-item">
+			    		<span class="nav-link">Logout</span>
+				    </li>              
+                {:else}
+				    <!-- Not Logged In -->
+				    <li class="nav-item">
+					    <a class:active = {currentPage == '/'} class="nav-link" aria-current="page" href="/">Home</a>
+				    </li>
+				    <li class="nav-item">
+					    <a class:active = {currentPage == '/about'} class="nav-link" href="/about">About</a>
+				    </li>
+				    <li class="nav-item">
+					    <a class:active = {currentPage == '/login'} class="nav-link" href="/login">Login</a>
+				    </li>
+				    <li class="nav-item">
+					    <a class:active = {currentPage == '/signup'} class="nav-link" href="/signup">Sign Up</a>
+				    </li>
+                {/if}  
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<style>
+	.nav-item > span {
+		cursor: pointer;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+    <title>BookLovers - About</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>About BookLovers</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptatem iusto perspiciatis iste, ipsa animi veniam laboriosam nam corporis exercitationem, rerum quidem tenetur ut pariatur impedit repellat deleniti delectus odio!
+        Sed velit voluptas expedita, veritatis enim itaque tempora quaerat ullam perspiciatis? Et voluptate quo, amet numquam perspiciatis dolor asperiores eos doloremque eaque deserunt maxime repellendus optio? Aliquid mollitia excepturi magnam!</p>
+    </div>
+</div>
