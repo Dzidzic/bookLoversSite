881aedd47e6ea12c8089d246651d8dae9cb42836
Dzidzic <mdzidzic07@gmail.com>
2024-08-04 10:57:32 +0200

    Message and Firebase Client SIde
    Added custom error/success message and Firebase Client Side initialize

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,28 @@
 <script>
     import 'bootstrap/dist/css/bootstrap.min.css';
     import Nav from '$lib/components/Nav.svelte';
+    import messageStore from '$lib/stores/message.store';
+    import '$lib/firebase/firebase.client';
+
+    $: messageTitle = $messageStore.type === 'error' ? "Error:" : "Success:";
+
+    function hideMessage(){
+        messageStore.hide();
+    }
 </script>
 
 <Nav />
 <main class="container">
+    {#if $messageStore.show}
+        <div class="row mt-3">
+            <div class="col">
+                <div class:alert-danger = {$messageStore.type === 'error'} class:alert-success = {$messageStore.type === 'success'}  class="alert alert-dismissible" role="alert">
+                    <strong>{messageTitle}</strong>
+                    {$messageStore.message}
+                    <button type="button" class="btn-close" aria-label="Close" on:click={hideMessage}/>
+                </div>
+            </div>
+        </div>
+    {/if}
     <slot />
 </main>
